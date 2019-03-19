ac1377424711be1c45042a736f5e7a5909342851
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-03-19 11:38:05 +0100

    Make tests more extensible

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -7,41 +7,44 @@ from typing import Any, List
 
 from table_validator import validate
 
-simple_template = [
-    ['', 'C_A', 'C_B', 'C_C'],  # Header
-    ['R_1', '{INT:REQUIRED}', '{INT:REQUIRED', '{INT:REQUIRED}'],
-]
 
-
-class TestTableValidator(unittest.TestCase):
+class TableValidatorMixin(unittest.TestCase):
     """Tests for ``table_validator``."""
 
+    TEMPLATE: List[List[Any]]
+
     def assertValidCandidate(self, candidate: List[List[Any]], msg=None):
-        self.assertTrue(validate(simple_template, candidate), msg=msg)
+        self.assertTrue(validate(self.TEMPLATE, candidate), msg=msg)
 
     def assertInvalidCandidate(self, candidate: List[List[Any]], msg=None):
-        self.assertFalse(validate(simple_template, candidate), msg=msg)
+        self.assertFalse(validate(self.TEMPLATE, candidate), msg=msg)
+
+
+class TestTableValidator(TableValidatorMixin):
+    """Tests for ``table_validator``."""
+
+    TEMPLATE = [
+        ['', 'C_A', 'C_B', 'C_C'],  # Header
+        ['R_1', '{INT:REQUIRED}', '{INT:REQUIRED}', '{INT:REQUIRED}'],
+    ]
 
     def test_missing_data(self):
-        candidate = [
+        self.assertInvalidCandidate([
             ['', 'C_A', 'C_B', 'C_C'],  # Header
             ['R_1', None, None, None],  # missing data
-        ]
-        self.assertInvalidCandidate(candidate)
+        ])
 
     def test_wrong_type(self):
-        candidate = [
+        self.assertInvalidCandidate([
             ['', 'C_A', 'C_B', 'C_C'],  # Header
             ['R_1', 'a', 'b', 'c'],  # apparent but wrong types
-        ]
-        self.assertInvalidCandidate(candidate)
+        ])
 
     def test_passes(self):
-        candidate = [
+        self.assertValidCandidate([
             ['', 'C_A', 'C_B', 'C_C'],  # Header
             ['R_1', 1, 2, 3],  # apparent but correct type
-        ]
-        self.assertValidCandidate(candidate)
+        ])
 
 
 if __name__ == '__main__':
