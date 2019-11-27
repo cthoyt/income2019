718be8f4afe80f160da58052f52c2c5cd543ef19
Wolfgang Müller <wolfgang.mueller@h-its.org>
2019-11-27 21:44:15 +0100

    More debugging output for better understanding of the code.

--- src/table_validator/validators.py
+++ src/table_validator/validators.py
@@ -10,18 +10,30 @@ __all__ = [
     'TemplateValidator',
 ]
 
-
 class TemplateValidator:
     def __init__(self, template):
+
+
+        print("were here");
+        print(self);
+        print(template);
+
         if isinstance(template, str):  # is a path
             with open(template) as file:
+                print("works?")
                 self.template = parse_tsv(file)
         elif isinstance(template, list):  # is pre-parsed
+            print("works2?")
             self.template = template
-        elif isinstance(template, TextIO):
+        elif True or isinstance(template, TextIO):# always.... does it break?
+            print("works3?")
             self.template = parse_tsv(template)
+        print("self template");
+        print(self.template);
 
     def validate(self, candidate: Union[list, TextIO]) -> bool:
+        print("in validate:");
+        print(self);
         if isinstance(candidate, list):
             return validate(self.template, candidate)
         return validate(self.template, parse_tsv(candidate))
